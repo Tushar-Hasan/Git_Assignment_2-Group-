a01e6553a4b53d3d8ac84b12d32b1be6298e9d22
Sumaiya Habib <sumaiya.habib@brainstation-23.com>
2024-04-26 12:00:40 +0600

    add operation

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+
+        scanner.close();
+    }
+}
